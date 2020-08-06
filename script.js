9a6b3beb2d9f8636d609ac96cc7d75345488371c
MatteoVollero <m.vollero120796@gmail.com>
2020-08-06 17:04:21 +0200

    Aggiusti

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var nome = prompt("Come ti chiami? ");
-var sesso = prompt("Inserire il sesso: ");
+var sesso = prompt("Inserire il sesso(maschio/femmina): ");
 
 if(sesso == "maschio" || sesso == "Maschio" || sesso == "MASCHIO"){
   document.getElementById('sesso').className = "blue";
-}else{
+}else if(sesso == "femmina" || sesso == "Femmina" || sesso == "FEMMINA"){
   document.getElementById('sesso').className = "red";
+}else {
+  alert("Inserimento del sesso errato");
 }
 
 document.getElementById('sesso').innerHTML = nome;
